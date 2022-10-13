77c1f3e14af3e01983e10e72c2e24ea6500a9096
Alexei <datlife2015@yandex.ru>
2022-10-13 13:37:10 +0300

    Показать таблицу квадратов чисел от 1 до N

--- 008/Program.cs
+++ 008/Program.cs
@@ -3,9 +3,9 @@ Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine() ?? "0");
 int a = 1;
 
-while (a<=N)
+while (a <= N)
 {
-    int b=a*a;
+    int b = a * a;
     Console.WriteLine($"Квадрат числа {a}, равен {b} ");
     a++;
 }
