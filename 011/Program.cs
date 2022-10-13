a5dcd589834648ebce706f1720fbe3c66021dabf
Alexei <datlife2015@yandex.ru>
2022-10-13 19:19:27 +0300

    Определить количество цифр в числе

---
+++ 011/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить количество цифр в числе
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int count = 0;
+
+if (num == 0)
+{
+    Console.WriteLine("Количество цифр в числе равно 1");
+}
+else
+{
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    Console.WriteLine($"Количество цифр в числе равно {count}");
+}
+
+
