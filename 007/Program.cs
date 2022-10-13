6a19a4d86e2950647afaa08497ab7c1721f42abe
Alexei <datlife2015@yandex.ru>
2022-10-13 13:19:21 +0300

    Найти третью цифру с начала числа и с конца числа или сообщить, что её нет. Математическое решение.

---
+++ 007/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти третью цифру с начала числа и с конца числа или сообщить, что её нет.
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+
+else
+{
+    int digit = num / 100 % 10;
+    Console.WriteLine($"Третья цифра с конца числа {digit}");
+    {
+    while (num > 1000)
+
+    num /= 10;
+    Console.WriteLine($"Третья цифра с начала числа {num % 10}");
+    }
+}
+    
+
